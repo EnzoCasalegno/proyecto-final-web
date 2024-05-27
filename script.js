61bcb059a91efcffe15a50d7f74a68298809cd5f
AlexisAllendez <163199846+AlexisAllendez@users.noreply.github.com>
2024-05-27 20:28:42 -0300

    Funcionalidad de carrousel
    Co-Authored-By: EnzoCasalegno <163201596+EnzoCasalegno@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 //Array que contiene las imagenes
 var imagenes = [
-    "image1.jpg",
-    "image2.jpg",
-    "image3.jpg",
-    "image4.jpg",
-    "image5.jpg"
+    "./carrousel/image1.jpg",
+    "./carrousel/image2.jpg",
+    "./carrousel/image3.jpg",
+    "./carrousel/image4.jpg",
+    "./carrousel/image5.jpg"
 ];
 
 //Declaracion de variables
 var index = 0;
-var carrouselImg = document.querySelector('carrousel-img');
-var btnIzq = document.querySelector('.izquierda');
-var btnDer = document.querySelector('.derecha');
+
 
 //Funcion para ver img
-function verImg(index) {
-    carrouselImg.src = imagenes[index];
+function verImg() {
+    let actImg = document.querySelector('.img');
+    actImg.src = imagenes[index];
 }
 //Funcion de btn para siguiente imagen
 function siguienteImg() {
-    index = (index + 1) % imagenes.length;
-    verImg(index);
+    if (index < imagenes.length - 1) {
+        index++;
+    }else{
+        index=0;
+    }
+    verImg();
 }
 //Funcion de btn para imagen previa
 function prevImg() {
-    index = (index - 1 + imagenes.length) % imagenes.length;
-    verImg(index);
+    if (index > 0) {
+        index--;
+    } else {
+        index = imagenes.length - 1;
+    }
+    verImg();
 }
 //Accion de botones
 btnIzq.addEventListener('click', prevImg);
